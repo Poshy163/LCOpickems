63eb412c09d9dd478ea48db0a3bd6c5b65905fa7
Joshua Leaper <poshernater@outlook.com>
2022-02-26 20:23:01 +1030

    Separated files, added updater/downloader

---
+++ App/LCOpickem/DatabaseInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace LCOpickem
+{
+    internal class DatabaseInfo
+    {
+        public static string ConnectionString = "mongodb+srv://Poshy:PoshyNew@userinfo.sungb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    }
+}
